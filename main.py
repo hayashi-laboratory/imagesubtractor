3368692c4acba77182d2953b359ad084e8947780
Chung-Kuan Chen <b97b01045@gmail.com>
2021-07-28 22:42:31 +0900

    modify license

--- main.py
+++ main.py
@@ -4,20 +4,6 @@
 Created on Tue Dec  5 09:58:05 2017
 Copyright (c) 2018, Taizo kawano
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-###########################################################################
-
 1 Run this script
 2 Click the open button and select image files directory
 3 Choose a roi col x row from drop down list or manually
